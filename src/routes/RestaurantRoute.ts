d8ca0cbbfb5827eefe4c2fea7db35eda0051baae
marchisDev <buiquangchicv0303@gmail.com>
2024-09-19 16:20:35 +0700

    adding checkout flow

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,6 +4,15 @@ import RestaurantController from '../controllers/RestaurantController';
 
 const router = express.Router();
 
+router.get('/:restaurantId', 
+    param('city')
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage('RestaurantId parameter must be a string and not empty'),
+    RestaurantController.getRestaurant
+);
+
 router.get('/search/:city', 
     param('city')
     .isString()
