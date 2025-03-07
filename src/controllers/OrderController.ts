9476e03e072876e5106e3c49f999289126c7b843
marchisDev <buiquangchicv0303@gmail.com>
2025-03-07 18:12:09 +0700

    fix redirect after payment

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -180,7 +180,7 @@ const createSession = async (
       orderId,
       restaurantId,
     },
-    success_url: `${FRONTEND_URL}/order-status?success=true`,
+    success_url: `${FRONTEND_URL}/order-status`,
     cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
   });
 
